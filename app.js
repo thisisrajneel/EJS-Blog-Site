fc0f1bfb8bc774552a4f10a5bca7a6a86ee7e17c
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2021-08-08 16:28:42 +0530

    challenge1

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
+app.get('/', (req, res) => {
+  res.render('home.ejs')
+})
 
 
 
