1a19fa8df427781181aa97e19267620b0df81cde
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2021-08-10 13:52:35 +0530

    added port variable

--- app.js
+++ app.js
@@ -47,6 +47,6 @@ app.post('/compose', (req, res) => {
   res.redirect('/')
 })
 
-app.listen(3000, function() {
+app.listen(3000 || process.env.PORT, function() {
   console.log("Server started on port 3000");
 });
