f9d52455aaf27b598bda294abc55003a98f21e30
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2021-08-10 18:51:24 +0530

    switched port positions

--- app.js
+++ app.js
@@ -47,6 +47,6 @@ app.post('/compose', (req, res) => {
   res.redirect('/')
 })
 
-app.listen(3000 || process.env.PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
